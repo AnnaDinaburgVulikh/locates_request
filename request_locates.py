86dae21e0c266d5f51c31711fdfa447be7510d88
Anna Dinaburg <anna.dinaburg@aquasec.com>
2023-09-09 20:54:54 +0300

    update to use class for requests

--- request_locates.py
+++ request_locates.py
@@ -2,37 +2,46 @@ import csv
 import random
 
 
-def read_request_from_csv(filename: str) -> list[dict]:
+class ClientRequest:
+    def __init__(self, client_name: str, symbol: str, number_of_locates_requested: int):
+        self.client_name = client_name
+        self.symbol = symbol
+        self.number_of_locates_requested = number_of_locates_requested
+        self.number_of_locates_approved = 0
+
+
+def read_request_from_csv(filename: str) -> list[ClientRequest]:
     client_requests = []
     with open(filename, 'r') as file:
         reader = csv.reader(file)
         headers = next(reader)
         for row in reader:
-            entry = {headers[i]: row[i] for i in range(len(headers))}
-            client_requests.append(entry)
+            new_client_request = ClientRequest(client_name=row[0], symbol=row[1],
+                                               number_of_locates_requested=int(row[2]))
+            client_requests.append(new_client_request)
     return client_requests
 
 
-def write_approved_request_to_csv(data_list: list, filename: str) -> None:
+def write_approved_request_to_csv(client_list: list[ClientRequest], filename: str) -> None:
     with open(filename, 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=data_list[0].keys())
-        writer.writeheader()
-        for row in data_list:
-            writer.writerow(row)
+        writer = csv.writer(file)
+        writer.writerow(['client_name', 'symbol', 'number_of_locates_approved'])
+        for client in client_list:
+            writer.writerow([client.client_name, client.symbol, client.number_of_locates_approved])
 
 
-def aggregate_requests_by_symbol(client_requests: list) -> dict:
+def aggregate_requests_by_symbol(client_list: list[ClientRequest]) -> dict:
     """
     aggregates all requests per symbol
-    :param client_requests: list of requests that include requested symbol and number_of_locates_requested
+    :param client_list: list of requests that include requested symbol and number_of_locates_requested
     :return: dict[str, int] of symbols and total of number_of_locates_requested per symbol
     """
     symbol_request_dict = {}
-    for client_request in client_requests:
-        if client_request['symbol'] in symbol_request_dict:
-            symbol_request_dict[client_request['symbol']] += int(client_request['number_of_locates_requested'])
+    for client_request in client_list:
+        if client_request.symbol in symbol_request_dict:
+            symbol_request_dict[client_request.symbol] += client_request.number_of_locates_requested
         else:
-            symbol_request_dict[client_request['symbol']] = int(client_request['number_of_locates_requested'])
+            symbol_request_dict[client_request.symbol] = client_request.number_of_locates_requested
     return symbol_request_dict
 
 
@@ -50,22 +59,22 @@ def request_locates(requested_locates: dict[str, int]) -> dict[str, int]:
     return approved_locates
 
 
-def distribute_locates(client_requests: list[dict],
+def distribute_locates(client_list: list[ClientRequest],
                        requested_locates: dict[str, int],
                        approved_locates: dict[str, int]) -> None:
     """
     Distributes the approved locates and update client requests based on the following logic:
     - approval by proportion to request
     - round up to 100 if possible
-    :param client_requests: list of client requests by symbol and number of locates
+    :param client_list: list of client requests by symbol and number of locates
     :param requested_locates: dict of requested locates by symbol
     :param approved_locates: dict of approved locates by symbol
     """
-    for client_request in client_requests:
-        symbol = client_request['symbol']
+    for client in client_list:
+        symbol = client.symbol
         total_approved_for_symbol = approved_locates.get(symbol)
         if total_approved_for_symbol:
-            client_requested = int(client_request['number_of_locates_requested'])
+            client_requested = client.number_of_locates_requested
             percent_of_total_request = client_requested / requested_locates[symbol]
             if percent_of_total_request == 1:
                 client_approved = total_approved_for_symbol
@@ -74,25 +83,22 @@ def distribute_locates(client_requests: list[dict],
                                       total_approved_for_symbol)
                 requested_locates[symbol] -= client_requested
                 approved_locates[symbol] -= client_approved
-        else:
-            client_approved = 0
-        client_request['number_of_locates_approved'] = client_approved
-        client_request.pop('number_of_locates_requested')
+            client.number_of_locates_approved = client_approved
 
 
 # Read all requests from the CSV file.
 client_requests = read_request_from_csv(filename='client_requests.csv')
 
 # Aggregate all the requests for each symbol.
-requests_by_symbol = aggregate_requests_by_symbol(client_requests=client_requests)
+requests_by_symbol = aggregate_requests_by_symbol(client_list=client_requests)
 
 # Call request_locates with the aggregated result.
 approved_requests = request_locates(requested_locates=requests_by_symbol)
 
 # Distribute the result to the clients.
-distribute_locates(client_requests=client_requests,
+distribute_locates(client_list=client_requests,
                    requested_locates=requests_by_symbol,
                    approved_locates=approved_requests)
 
 # write results to CSV file
-write_approved_request_to_csv(data_list=client_requests, filename='approved_requests.csv')
+write_approved_request_to_csv(client_list=client_requests, filename='approved_requests.csv')
